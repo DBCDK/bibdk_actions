f491f585694bca60286066923449e928b39dd0c5
Søren Vibjerg <vibjerg@gmail.com>
2013-02-27 16:56:12 +0100

    basic print functionality

---
+++ theme/page--print.tpl.php
@@ -0,0 +1,20 @@
+<div id="popup">
+    <div class="element-wrapper">
+        <div class="element">
+
+          <?php if (!empty($title)): ?>
+            <h1 id="title"><?php print $title; ?></h1>
+          <?php endif; ?>
+
+          <?php if (!empty($messages)): ?>
+            <div id="messages">
+              <?php print $messages; ?>
+            </div>
+          <?php endif; ?>
+
+          <?php print render($page['content']); ?>
+
+        </div>
+    </div>
+</div>
+<!-- #popup -->
