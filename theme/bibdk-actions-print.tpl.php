048d4d9142d3a261dfe35cde5e37f5793fe172d4
Søren Vibjerg <vibjerg@gmail.com>
2013-02-28 13:34:46 +0100

    print template added

---
+++ theme/bibdk-actions-print.tpl.php
@@ -0,0 +1,10 @@
+<div class="print-wrapper">
+<h6><?php echo $title ?></h6>
+<div><?php echo $type ?></div>
+<div><?php echo $abstract ?></div>
+<div><?php echo $subjects ?></div>
+
+        <table>
+          <?php echo drupal_render($fields); ?>
+        </table>
+</div>
--- theme/page--print.tpl.php
+++ theme/page--print.tpl.php
@@ -1,20 +1,7 @@
-<div id="popup">
-    <div class="element-wrapper">
-        <div class="element">
-
+<div id="print">
           <?php if (!empty($title)): ?>
             <h1 id="title"><?php print $title; ?></h1>
           <?php endif; ?>
-
-          <?php if (!empty($messages)): ?>
-            <div id="messages">
-              <?php print $messages; ?>
-            </div>
-          <?php endif; ?>
-
           <?php print render($page['content']); ?>
-
-        </div>
-    </div>
 </div>
 <!-- #popup -->
